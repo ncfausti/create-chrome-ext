ebf3ccb2b35d56a3b0434430b51dd8fbf8682a7b
Nick Fausti <ncfausti@gmail.com>
2023-02-15 17:35:50 -0500

    Fix uncaught message error by sending a response and returning true inside onMessage.addListener callback.

--- gtr/src/background/index.ts
+++ gtr/src/background/index.ts
@@ -1,18 +1,17 @@
 console.info('chrome-ext template-react-ts background script')
 // add a listener for the toggle-app command specified in manifest.json
-chrome.commands.onCommand.addListener(function(command) {
-  if (command === "toggle-app") {
-    (async () => {
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'toggle-app') {
+    ;(async () => {
       //send a message directly to the current active tab from background
-      const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+      const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true })
 
       if (tab.id) {
         // that message will be to toggle map
-        const response = await chrome.tabs.sendMessage(tab.id, {message: 'toggle-map'});
-        return true;
+        const response = await chrome.tabs.sendMessage(tab.id, { message: 'toggle-map' })
       }
-      return true;
-    })();
+      return true
+    })()
   }
-});
+})
 export {}
--- gtr/src/content/index.ts
+++ gtr/src/content/index.ts
@@ -1,80 +1,139 @@
 console.info('chrome-ext template-react-ts content script')
-const iframe = document.createElement('iframe');
-// iframe.src = 'https://main.dy6d4zldr783d.amplifyapp.com/';
+const iframe = document.createElement('iframe')
+iframe.id = 'map-app-xyz'
+iframe.src = 'http://localhost:3001'
+// setInterval was getting cleared (or stopped running for some other reason) when
+// it was lower on the page. Something to look out for incase resizing stops working
+setInterval(function () {
+  // Why does this not get run all the time?
+  console.log('checking...')
+  const [hostMap] = Array.from(document.getElementsByTagName('iframe')).filter(
+    (el) => el.id !== 'map-app-xyz' && area(el.getBoundingClientRect()) > 500,
+  )
+  if (!hostMap) {
+    return
+  }
 
+  if (window.location.search !== previousSearch) {
+    // Query parameters have changed
+    previousSearch = window.location.search
+    console.log(previousSearch)
+
+    // update overlay map coords and zoom here
+  }
+
+  var checkRect = hostMap.getBoundingClientRect()
+  if (checkRect.width !== previousMapWidth || checkRect.height !== previousMapHeight) {
+    previousMapWidth = checkRect.width
+    previousMapHeight = checkRect.height
+
+    console.log('map size changed:')
+    console.log(checkRect.height, checkRect.width)
+
+    iframe.style.height = checkRect.height.toString() + 'px'
+    iframe.style.width = checkRect.width.toString() + 'px'
+    // update overlay map height & width here
+  }
+
+  if (checkRect.x !== previousMapX || checkRect.y !== previousMapY) {
+    previousMapX = checkRect.x
+    previousMapY = checkRect.y
+    console.log('map position changed:')
+    console.log(checkRect.x, checkRect.y)
+    // update overlay map x, y position on page here
+    const topOffset = checkRect.y.toString() + 'px'
+    const leftOffset = checkRect.x.toString() + 'px'
+    console.log(topOffset)
+    console.log(leftOffset)
+
+    iframe.style.top = topOffset
+    iframe.style.left = leftOffset
+  }
+}, 1000)
+// iframe.src = 'https://main.dy6d4zldr783d.amplifyapp.com/';
+console.log('HEREEEEEEEEEEEE')
+// setInterval(()=>console.log('interval says hi'), 500);
 // iframe needs to be embedded on main window page (e.g. airbnb.com)
 // as opposed to the chrome extension window bc the chrome extension window
 // functions as a Chrome Browser window and thus doe not allow for transparent views
-iframe.src = 'http://localhost:3001';
-iframe.id = 'map-app-xyz';
-document.body.appendChild(iframe);
-iframe.style.position = "absolute";
-iframe.style.top = '0px';
-iframe.style.left = '0px';
-iframe.style.minHeight = '1000px';
-iframe.style.width = '100%';
-iframe.style.display = 'none';
-iframe.style.opacity = '70%';
+document.body.appendChild(iframe)
+iframe.style.position = 'absolute'
+iframe.style.top = '0px'
+iframe.style.left = '0px'
+// iframe.style.minHeight = '1000px';
+// iframe.style.width = '100%';
+iframe.style.display = 'none'
+iframe.style.opacity = '70%'
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log('content: new msg received, showing message now...')
-    console.log(request.message);
-      if (request.message === 'toggle-map') {
-        if (iframe.style.display === 'none') {
-          iframe.style.display = 'block';
-        }
-        else {
-          iframe.style.display = 'none';
-        }
-      }
-      return true;
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log('content: new msg received, showing message now...')
+  console.log(request.message)
+  if (request.message === 'toggle-map') {
+    if (iframe.style.display === 'none') {
+      iframe.style.display = 'block'
+    } else {
+      iframe.style.display = 'none'
+    }
   }
-);
-const area = (boundingRect: {height: number, width: number}) => boundingRect.height * boundingRect.width;
-const [hostMap] = Array.from(document.getElementsByTagName('iframe')).filter(
-  el => el.id !== 'map-app-xyz' && area(el.getBoundingClientRect()) > 500);
-console.log(hostMap);
+  sendResponse({})
+  return true
+})
+
+const area = (boundingRect: { height: number; width: number }) =>
+  boundingRect.height * boundingRect.width
 
 // I should be able to create custom events for both of these instead of
 // polling via setInterval:
 
 // listen for changes to url to get lat, lng, and zoom changes
-var previousSearch = window.location.search;
+var previousSearch = window.location.search
 
 // listen for changes to boundingbox
-var mapRect = hostMap.getBoundingClientRect();
-var previousMapArea = area(mapRect);
-var [previousMapX, previousMapY] = [mapRect.x, mapRect.y];
+var mapRect = hostMap.getBoundingClientRect()
+var [previousMapWidth, previousMapHeight] = [mapRect.width, mapRect.height]
+var [previousMapX, previousMapY] = [mapRect.x, mapRect.y]
 
-setInterval(function() {
-  if (window.location.search !== previousSearch) {
-    // Query parameters have changed
-    previousSearch = window.location.search;
-    console.log(previousSearch);
-    console.log()
+// setInterval(function() {
+//   // Why does this not get run all the time?
+//   console.log('checking...');
 
-    // update overlay map coords and zoom here
-  }
+//   if (window.location.search !== previousSearch) {
+//     // Query parameters have changed
+//     previousSearch = window.location.search;
+//     console.log(previousSearch);
 
-  var checkRect = hostMap.getBoundingClientRect();
-  if (area(checkRect) !== previousMapArea) {
-    previousMapArea = area(checkRect);
-    console.log("map size changed:")
-    console.log(checkRect.height, checkRect.width);
+//     // update overlay map coords and zoom here
+//   }
 
-    // update overlay map height & width here
-  }
+//   var checkRect = hostMap.getBoundingClientRect();
+//   if (checkRect.width !== previousMapWidth || checkRect.height !== previousMapHeight) {
+//     previousMapWidth = checkRect.width;
+//     previousMapHeight = checkRect.height;
 
-  if (checkRect.x !== previousMapX || checkRect.y !== previousMapY) {
-    previousMapX = checkRect.x;
-    previousMapY = checkRect.y;
-    console.log("map position changed:")
-    console.log(checkRect.x, checkRect.y);
-    // update overlay map x, y position on page here
-  }
+//     console.log("map size changed:")
+//     console.log(checkRect.height, checkRect.width);
 
-}, 500);
+//     iframe.style.height = checkRect.height.toString() + "px";
+//     iframe.style.width = checkRect.width.toString() + "px";
+//     // update overlay map height & width here
+//   }
+
+//   if (checkRect.x !== previousMapX || checkRect.y !== previousMapY) {
+//     previousMapX = checkRect.x;
+//     previousMapY = checkRect.y;
+//     console.log("map position changed:")
+//     console.log(checkRect.x, checkRect.y);
+//     // update overlay map x, y position on page here
+//     const topOffset = checkRect.y.toString() + "px";
+//     const leftOffset = checkRect.x.toString() + "px";
+//     console.log(topOffset);
+//     console.log(leftOffset);
+
+//     iframe.style.top = topOffset;
+//     iframe.style.left = leftOffset;
+//   }
+
+// }, 100);
 
 // How can I have separate controls (or intercept the controls
 // that fire on google maps?
